1e93d766d05b502f6b39de04607340db2e0415c3
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-08-07 21:51:14 +0500

    Create .asm

---
+++ Read a character/.asm
@@ -0,0 +1,26 @@
+;Read a character, and display it at the T'.ext position on the same line.
+
+
+.model small
+.stack 100h
+.386
+.data
+.code
+main proc
+	mov ax,@data
+	mov ds,ax
+	
+	mov ah,01
+	int 21h
+	mov bl,al
+	
+	mov dl,bl
+	mov ah,02
+	int 21h
+	
+	mov ah,4ch
+	int 21h
+	
+	
+main endp
+end main
