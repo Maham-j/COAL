481b413bdc90e6a256357150d08e530c148dd2d0
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-08-09 23:20:48 +0500

    Update .asm

--- character count/.asm
+++ character count/.asm
@@ -1,37 +1,38 @@
-;character count
+; Character Count Program
+
 .MODEL SMALL
 .STACK 100h
+
 .data
-str1 db 100 dup('$')
-var1 db 0
+str1 db 100 dup('$')   ; Buffer for input
+var1 db 0              ; Character counter
+
 .CODE
 MAIN PROC
-    MOV AX, @DATA  
+    MOV AX, @DATA      ; Initialize data segment
     MOV DS, AX
-	lea si,str1
-	
-again:
-	mov ah,01
-	int 21h
-	mov bl,al
-	
-	cmp bl,13
-	je exit
-	
-	mov [si],bl
-	inc si
-	inc var1
-	jmp again
-	
-	
-	
-exit:
-	add var1,'0'
-	mov dl,var1
-	mov ah,02
-	int 21h
+    LEA SI, str1       ; Load buffer address
 
-    MOV AH, 4Ch    
+again:
+    MOV AH, 01         ; Read character
+    INT 21h
+    MOV BL, AL         ; Store character in BL
+    
+    CMP BL, 13         ; Check for Enter key
+    JE exit            ; Exit if Enter is pressed
+    
+    MOV [SI], BL       ; Save character in buffer
+    INC SI             ; Move to next position
+    INC var1           ; Increment counter
+    JMP again          ; Repeat
+
+exit:
+    ADD var1, '0'      ; Convert count to ASCII
+    MOV DL, var1       ; Load count in DL
+    MOV AH, 02         ; Display count
+    INT 21h
+
+    MOV AH, 4Ch        ; Exit program
     INT 21h
 
 MAIN ENDP
