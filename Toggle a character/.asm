3fc405c9f6d736273552f6f6517180792feb618c
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-08-18 22:02:28 +0500

    Create .asm

---
+++ Toggle a character/.asm
@@ -0,0 +1,27 @@
+;Read a character, and display it at the the next position on the same line.
+
+
+.model small
+.stack 100h
+.386
+.data
+.code
+main proc
+	mov ax,@data
+	mov ds,ax
+	
+	mov ah,01
+	int 21h
+	mov bl,al
+	
+	xor bl,20h
+	mov dl,bl
+	mov ah,02
+	int 21h
+	
+	mov ah,4ch
+	int 21h
+	
+	
+main endp
+end main
