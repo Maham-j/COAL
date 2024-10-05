a0b9c6c3b19c4915710c4398fbd44f7f69cd592a
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-05 19:38:37 +0500

    Create .asm

---
+++ ArrayReverse/.asm
@@ -0,0 +1,44 @@
+.model small
+.stack 100h
+.386
+.data
+array db 100 dup(0)
+new db 10,13,'$'
+.CODE
+main proc
+   
+	mov ax,@data
+	mov ds,ax
+	
+	xor cx,cx
+	
+again:
+	mov ah,01
+	int 21h
+	
+	cmp al,13
+	je reverse
+	
+	push ax
+	inc cx
+	jmp again
+
+end_read:	
+	lea si,new
+	mov ah,02
+	int 21h
+	
+reverse:
+	pop dx
+	mov ah,02
+	int 21h
+	dec cx
+	cmp cx,0
+	jne reverse
+	
+
+    MOV AH, 4Ch   
+    INT 21h
+
+main endp
+end main 
